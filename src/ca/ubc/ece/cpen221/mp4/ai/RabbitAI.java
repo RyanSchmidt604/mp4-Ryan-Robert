0d69ea661abc4b92579e22255d3c8d225e01f3d9
Robert Drinnan <robert.drinnan@gmail.com>
2015-11-05 19:38:17 -0800

    Added a working Rabbit AI

--- src/ca/ubc/ece/cpen221/mp4/ai/RabbitAI.java
+++ src/ca/ubc/ece/cpen221/mp4/ai/RabbitAI.java
@@ -7,11 +7,13 @@ import ca.ubc.ece.cpen221.mp4.ArenaWorld;
 import ca.ubc.ece.cpen221.mp4.Direction;
 import ca.ubc.ece.cpen221.mp4.Location;
 import ca.ubc.ece.cpen221.mp4.Util;
+import ca.ubc.ece.cpen221.mp4.World;
 import ca.ubc.ece.cpen221.mp4.commands.BreedCommand;
 import ca.ubc.ece.cpen221.mp4.commands.Command;
 import ca.ubc.ece.cpen221.mp4.commands.EatCommand;
 import ca.ubc.ece.cpen221.mp4.commands.MoveCommand;
 import ca.ubc.ece.cpen221.mp4.commands.WaitCommand;
+import ca.ubc.ece.cpen221.mp4.items.Grass;
 import ca.ubc.ece.cpen221.mp4.items.Item;
 import ca.ubc.ece.cpen221.mp4.items.animals.ArenaAnimal;
 import ca.ubc.ece.cpen221.mp4.items.animals.Fox;
@@ -31,7 +33,44 @@ public class RabbitAI extends AbstractAI {
 
 	@Override
 	public Command getNextAction(ArenaWorld world, ArenaAnimal animal) {
-		// TODO: Change this. Implement your own AI rules.
-		return new WaitCommand();
+		Set<Item> surroundings = world.searchSurroundings(animal);
+		for (Item item : surroundings) {
+			if (item instanceof Fox) {
+				Direction foxDirection = Util.getDirectionTowards(animal.getLocation(), item.getLocation());
+				if (foxDirection == Direction.NORTH) {
+					foxDirection = Direction.SOUTH;
+				}
+				if (foxDirection == Direction.EAST) {
+					foxDirection = Direction.WEST;
+				}
+				if (foxDirection == Direction.SOUTH) {
+					foxDirection = Direction.NORTH;
+				}
+				if (foxDirection == Direction.WEST) {
+					foxDirection = Direction.EAST;
+				}
+				Location newLocation = new Location(animal.getLocation(),
+						Util.getDirectionTowards(animal.getLocation(), item.getLocation()));
+				new MoveCommand(animal, newLocation);
+			}
+		}
+		for (Item item : surroundings) {
+			if (item instanceof Grass) {
+				if (animal.getLocation().getDistance(item.getLocation()) == 1) {
+					return new EatCommand(animal, item);
+				} else {
+					Location newLocation = new Location(animal.getLocation(),
+							Util.getDirectionTowards(animal.getLocation(), item.getLocation()));
+					new MoveCommand(animal, newLocation);
+				}
+			}
+		}
+		if (animal.getEnergy() >= animal.getMaxEnergy()) {
+			Location breedLocation = Util.getRandomEmptyAdjacentLocation((World) world, animal.getLocation());
+			if (Util.isValidLocation(world, breedLocation)) {
+				return new BreedCommand(animal.breed(), breedLocation);
+			}
+		}
+		return new MoveCommand(animal, Util.getRandomEmptyAdjacentLocation((World) world, animal.getLocation()));
 	}
 }
